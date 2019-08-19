608f58d9907c6a47d690177aed516024054728cb
qinghuan <[251064362@qq.com]>
2019-08-19 14:48:53 +0800

    ddd

--- denglu/common/get_token.py
+++ denglu/common/get_token.py
@@ -4,17 +4,17 @@ from ruamel import yaml
 from urllib3.exceptions import InsecureRequestWarning, InsecurePlatformWarning
 from denglu.ReadExecle import openExecle
 
-def login(url,pram):
+def login(url, pram):
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     headers = {
     "Content-Type":"application/json; charset=utf-8",
     "User-Agent":"Dalvik/2.1.0 (Linux; U; Android 8.1.0; vivo X21i A Build/O11019)"
     }
-    r = requests.post(url,data=pram,headers=headers,verify=False)
+    r = requests.post(url, data=pram, headers=headers, verify=False)
     session = r.headers["Set-Cookie"]
     json = r.json()
     userid= json["data"]["userId"]
-    return session,userid
+    return session, userid
 
 def write_token(session):
     curpath = os.path.dirname(os.path.dirname(__file__))
@@ -22,8 +22,9 @@ def write_token(session):
     t = {"Cookie":session}
     with open(ypath, "w", encoding="utf-8") as f:
         yaml.dump(t, f, Dumper=yaml.RoundTripDumper)
+
 if __name__=="__main__":
     readdata = openExecle(r'..\Data\testcase.xlsx')
     url = readdata.sheet.row(10)[3].value
     pram = readdata.sheet.row(10)[6].value
-    print(login(url,pram))
+    print(login(url, pram))
