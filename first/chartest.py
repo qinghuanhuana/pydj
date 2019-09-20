c22ec7ba5227d59fcf54c33ea151f99043bcc5cc
qinghuan <[13662673020@163.com]>
2019-09-20 15:14:36 +0800

    sys.path.append(os.path.dirname(os.getcwd()))
    sys.path.append(os.path.dirname(os.getcwd()))

--- first/chartest.py
+++ first/chartest.py
@@ -1,5 +1,32 @@
+import time
+import random
 
-str1 = input()
-str2 = input()
-count = str1.count(str2)
-print(count)
+while True:
+    i = random.randint(0, 2**32) % 100
+    print ('input you guess:\n')
+    start = time.clock()
+    b = time.time()
+    guess = int(input('input your guess:\n'))
+    while guess != i:
+        if guess > i:
+            print('smaller')
+            guess = int(input('input your guess:\n'))
+        elif guess == i:
+            break
+        else:
+            print('bigger')
+            guess = int(input('input your guess:\n'))
+
+    end = time.clock()
+    d = time.time()
+    var = (end - start) / 18.2
+    print(var)
+
+    if var < 15:
+        print('clever')
+    elif var < 25:
+        print('normal')
+    else:
+        print('stupid')
+    print('congresdultions')
+    print('number you gues is %d' % i)
